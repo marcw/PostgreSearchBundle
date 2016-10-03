6cef9192af3f1aec521c9745e8f55bf17e7d19d4
Marc Weistroff <marc@weistroff.net>
2016-10-03 12:15:17 +0200

    CS

--- DQL/TsqueryFunction.php
+++ DQL/TsqueryFunction.php
@@ -1,18 +1,19 @@
 <?php
+
 namespace Ddmaster\PostgreSearchBundle\DQL;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 
 /**
- * TsqueryFunction ::= "TSQUERY" "(" StringPrimary "," StringPrimary "[, " StringPrimary "])"
+ * TsqueryFunction ::= "TSQUERY" "(" StringPrimary "," StringPrimary "[, " StringPrimary "])".
  */
 class TsqueryFunction extends FunctionNode
 {
     public $fieldName = null;
     public $queryString = null;
     public $configuration = null;
-    
+
     public function parse(\Doctrine\ORM\Query\Parser $parser)
     {
         $parser->match(Lexer::T_IDENTIFIER);
@@ -20,26 +21,24 @@ class TsqueryFunction extends FunctionNode
         $this->fieldName = $parser->StringPrimary();
         $parser->match(Lexer::T_COMMA);
         $this->queryString = $parser->StringPrimary();
-        
+
         $lexer = $parser->getLexer();
         if ($lexer->isNextToken(Lexer::T_COMMA)) {
             $parser->match(Lexer::T_COMMA);
             $this->configuration = $parser->StringPrimary();
         }
-        
+
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
         if ($this->configuration) {
-            $stmnt = $this->fieldName->dispatch($sqlWalker) .  " @@ to_tsquery(" . $this->configuration->dispatch($sqlWalker) . ", " . $this->queryString->dispatch($sqlWalker) . ")";
+            $stmnt = $this->fieldName->dispatch($sqlWalker).' @@ to_tsquery('.$this->configuration->dispatch($sqlWalker).', '.$this->queryString->dispatch($sqlWalker).')';
+        } else {
+            $stmnt = $this->fieldName->dispatch($sqlWalker).' @@ to_tsquery('.$this->queryString->dispatch($sqlWalker).')';
         }
-        else {
-            $stmnt = $this->fieldName->dispatch($sqlWalker) .  " @@ to_tsquery(" . $this->queryString->dispatch($sqlWalker) . ")";
-        }
-        
+
         return $stmnt;
-        	
     }
 }
--- DQL/TsrankFunction.php
+++ DQL/TsrankFunction.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Ddmaster\PostgreSearchBundle\DQL;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 
 /**
- * TsrankFunction ::= "TSRANK" "(" StringPrimary "," StringPrimary ")"
+ * TsrankFunction ::= "TSRANK" "(" StringPrimary "," StringPrimary ")".
  */
 class TsrankFunction extends FunctionNode
 {
@@ -17,15 +18,15 @@ class TsrankFunction extends FunctionNode
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
         $this->fieldName = $parser->StringPrimary();
-        $parser->match(Lexer::T_COMMA); 
+        $parser->match(Lexer::T_COMMA);
         $this->queryString = $parser->StringPrimary();
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
-        return 
-        	'ts_rank(' . $this->fieldName->dispatch($sqlWalker) . ', ' .
-            ' to_tsquery(' . $this->queryString->dispatch($sqlWalker) . '))';
+        return
+            'ts_rank('.$this->fieldName->dispatch($sqlWalker).', '.
+            ' to_tsquery('.$this->queryString->dispatch($sqlWalker).'))';
     }
 }
